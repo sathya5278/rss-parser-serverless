6121d94a08a4defdf6ee4c5164fcb3ff56121fcf
sathya5278 <sathyamohan012@gmail.com>
2021-01-02 14:36:05 +0530

    Error handling and cors

--- src/api.js
+++ src/api.js
@@ -10,27 +10,31 @@ app.use(cors());
 app.use(bodyParser.json());
 
 router.post("/rssfeeds", (req, res) => {
-  const FEED_LIST = req.body.feeds;
-  //res.json(req.body);
+  if (req.body.feeds) {
+    const FEED_LIST = req.body.feeds;
+    //res.json(req.body);
 
-  let parser = new Parser();
+    let parser = new Parser();
 
-  const feedRequests = FEED_LIST.map((feed) => {
-    return parser.parseURL(feed);
-  });
-
-  Promise.all(feedRequests)
-    .then((response) => {
-      // res.setHeader("Access-Control-Allow-Origin", "*");
-      // res.setHeader('Access-Control-Allow-Origin', 'some-domain-to-allow.com');
-      // res.header("Access-Control-Allow-Methods", "POST");
-      res.json(response);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(400);
-      res.send({ message: error });
+    const feedRequests = FEED_LIST.map((feed) => {
+      return parser.parseURL(feed);
     });
+
+    Promise.all(feedRequests)
+      .then((response) => {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        // res.setHeader('Access-Control-Allow-Origin', 'some-domain-to-allow.com');
+        res.header("Access-Control-Allow-Methods", "POST");
+        res.json(response);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(400);
+        res.send({ message: error });
+      });
+  } else {
+    res.send("No feed urls found");
+  }
 });
 
 app.use(`/.netlify/functions/api`, router);
